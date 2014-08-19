34d67fac9626e5fea260be70a16451328c1e40d1
Julia <juliajtan@gmail.com>
2014-08-19 11:12:58 +0100

    Booking can have a finish row and finish seat

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,11 +1,13 @@
 class Booking
 
-  attr_reader :id, :start_row, :start_seat
+  attr_reader :id, :start_row, :start_seat, :finish_row, :finish_seat
 
-  def initialize(id, start_row, start_seat)
+  def initialize(id, start_row, start_seat, finish_row=nil, finish_seat=nil)
     @id = id
     @start_row = start_row
     @start_seat = start_seat
+    @finish_row = finish_row
+    @finish_seat = finish_seat
   end
 
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -3,7 +3,7 @@ require 'booking'
 describe Booking do
 
   let(:seat1) { Seat.new(1, 1) }
-  let(:booking) { Booking.new(0, 5, 6)}
+  let(:booking) { Booking.new(0, 5, 6, 5, 7)}
   # 0,77:23,77:24
 
   context 'when initialized' do
@@ -18,5 +18,15 @@ describe Booking do
     it 'has a starting seat' do
       expect(booking.start_seat).to eq 6
     end
+
+    it 'can have a finishing row' do
+      expect(booking.finish_row).to eq 5
+    end
+
+    it 'can have a finishing seat' do
+      expect(booking.finish_seat).to eq 7
+    end
   end
+
+  
 end
