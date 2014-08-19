a2d23577a17d0a19f792f5405cf721506f7dc557
Julia <juliajtan@gmail.com>
2014-08-19 11:06:24 +0100

    Booking must be initialized with a start row

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,9 +1,10 @@
 class Booking
 
-  attr_reader :id
+  attr_reader :id, :start_row
 
-  def initialize(id)
+  def initialize(id, start_row)
     @id = id
+    @start_row = start_row
   end
 
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -3,12 +3,16 @@ require 'booking'
 describe Booking do
 
   let(:seat1) { Seat.new(1, 1) }
-  let(:booking) { Booking.new(0)}
+  let(:booking) { Booking.new(0, 5)}
   # 0,77:23,77:24
 
   context 'when initialized' do
     it 'has a unique id' do
       expect(booking.id).to eq 0
+    end
+
+    it 'has a starting row' do
+      expect(booking.start_row).to eq 5
     end
   end
 end
